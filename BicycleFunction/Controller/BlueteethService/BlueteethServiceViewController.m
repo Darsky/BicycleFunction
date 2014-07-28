98494d761d1fd6e65b7d46b0df84b1ce7288e8a7
Xin.Xion <543207071@qq.com>
2014-07-29 00:08:21 +0800

    Blueteeth Test

---
+++ BicycleFunction/Controller/BlueteethService/BlueteethServiceViewController.m
@@ -0,0 +1,13 @@
+//
+//  BlueteethServiceViewController.m
+//  BicycleFunction
+//
+//  Created by Darsky on 14/7/27.
+//  Copyright (c) 2014年 Darsky. All rights reserved.
+//
+
+#import "BlueteethServiceViewController.h"
+
+@implementation BlueteethServiceViewController
+
+@end
---
+++ BicycleFunction/Model/BlueteethConnect/BlueteethService.m
@@ -0,0 +1,13 @@
+//
+//  BlueteethService.m
+//  BicycleFunction
+//
+//  Created by Darsky on 14/7/27.
+//  Copyright (c) 2014年 Darsky. All rights reserved.
+//
+
+#import "BlueteethService.h"
+
+@implementation BlueteethService
+
+@end
